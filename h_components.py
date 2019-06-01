dcc1272671a53a22550b944380a34f42f85822bb
srijaniiserprinceton <sbdas@princeton.edu>
2019-06-01 17:00:01 +0530

    Adding a few comments to lookout for rectifications

--- h_components.py
+++ h_components.py
@@ -16,10 +16,13 @@ class getHcomps:
         
         t = np.arange(-np.max(np.abs(self.s)),np.max(np.abs(self.s))+1,1)
         mumu,nunu,ss,tt = np.meshgrid(self.mu,self.nu,self.s,t,indexing='ij')
+
         wig_calc = np.vectorize(fn.wig)
+
+        #signs to be checked
         wig1 = wig_calc(1,ss,1,mumu,-(mumu+nunu),nunu)
         wig2 = wig_calc(1,ss,1,0,tt,0)
-
+        #factor of 9 needs to be replaced with generic l expression
         H = np.sqrt((9/(4*np.pi))*(2*ss+1))*wig1*wig2
 
         HH = H[:,:,:,:,np.newaxis]*self.r[np.newaxis,:]
