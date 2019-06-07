f18bf3dfb1f32e3b6210a919a89545a2d9556144
Tuneer Chakraborty <tuneer.ch@gmail.com>
2019-06-07 20:19:41 +0530

    make get_kernels cleaner

--- get_kernels.py
+++ get_kernels.py
@@ -11,58 +11,38 @@ class Hkernels:
     """This class handles l parameters of the kernel"""
     #setting up shorthand repeatedly used in kernel evaluation
 
-    def __init__(self,l,l_,r,rpts,ss1=0,ss2=0):
+    def __init__(self,n_,l_,n,l,s,r_start,r_end):
+        self.n = n
         self.l = l
+        self.n_ = n_
         self.l_ = l_
-        self.ss = 0
-        self.s = 0
-        self.rpts = rpts
-        self.r = r
-
-    def isol_multiplet(self,n,l,s):
-        n_ = n
-        l_ = l
+        r_full = np.loadtxt('r.dat')
+        self.r = r_full[r_start:r_end]
         m = np.arange(-l,l+1,1)
-        m_ = np.arange(-l,l+1,1)
-
-        mm,mm_,ss1 = np.meshgrid(m,m_,s,indexing='ij')
-        __,__,ss2,__ = np.meshgrid(m,m,s,self.r,indexing='ij')
-
-        self.ss = ss1
-        self.s = ss2[0,0,:,:]
-
-        Bmm,B0m,B00,Bpm,Bpp,B0p = self.ret_kerns(l,self.ss,l_,mm,mm_,n,n_)
-        return Bmm,B0m,B00,Bpm,Bpp,B0p
+        m_ = np.arange(-l_,l_+1,1)
+        #ss is m X m X s dim (outer)
+        self.mm_, self.mm, self.ss_o = np.meshgrid(m_,m,s, indexing = 'ij')
+        #ss_in is s X r dim (inner)
+        self.ss_i,__ = np.meshgrid(s,self.r, indexing = 'ij')
+        self.r_range = r_start,r_end
 
     def wig_red_o(self,m1,m2,m3):
-        '''3j symbol with upper row fixed'''
+        '''3j symbol with upper row fixed (outer)'''
         wig_vect = np.vectorize(fn.wig)
-        return wig_vect(self.l_,self.ss,self.l,m1,m2,m3)
+        return wig_vect(self.l_,self.ss_o,self.l,m1,m2,m3)
 
     def wig_red(self,m1,m2,m3):
-        '''3j symbol with upper row fixed'''
+        '''3j symbol with upper row fixed (inner)'''
         wig_vect = np.vectorize(fn.wig)
-        return wig_vect(self.l_,self.s,self.l,m1,m2,m3)
+        return wig_vect(self.l_,self.ss_i,self.l,m1,m2,m3)
 
-    def ret_kerns(self,l,s,l_,m,m_,n,n_):
-    
-    
-    	########################################################
-    	
-    	m = np.arange(-l,l+1,1)
-        m_ = np.arange(-l_,l_+1,1)
-
-        __,__,ss2,__ = np.meshgrid(m,m,s[0,0,:],self.r[-700],indexing='ij')
-
-        self.ss = s
-        self.s = ss2[0,0,:,:]
-    	
-    	########################################################
-
+    def ret_kerns(self):
+        n,l,m,n_,l_,m_ = self.n, self.l, self.mm, self.n_, self.l_, self.mm_
+        r_start , r_end = self.r_range
         nl = fn.find_nl(n,l)
         nl_ = fn.find_nl(n_,l_)
 
-        len_m, len_m_, len_s = np.shape(s)
+        len_m, len_m_, len_s = np.shape(self.ss_o)
 
         #Savitsky golay filter for smoothening
         window = 45  #must be odd
@@ -75,21 +55,20 @@ class Hkernels:
         eig_dir = (getcwd() + '/eig_files')
         Ui,Vi = fn.load_eig(n,l,eig_dir)
         Ui_,Vi_= fn.load_eig(n_,l_,eig_dir)
-        r = np.loadtxt('r.dat')
         rho = np.loadtxt('rho.dat')
 
         #slicing the radial function acoording to radial grids
-        r = r[-self.rpts:]
-        rho = rho[-self.rpts:]
-        Ui = Ui[-self.rpts:]
-        Vi = Vi[-self.rpts:]
-        Ui_ = Ui_[-self.rpts:]
-        Vi_ = Vi_[-self.rpts:]
+        r = self.r
+        rho = rho[r_start:r_end]
+        Ui = Ui[r_start:r_end]
+        Vi = Vi[r_start:r_end]
+        Ui_ = Ui_[r_start:r_end]
+        Vi_ = Vi_[r_start:r_end]
 
         tstamp()
         om = np.vectorize(fn.omega)
-        parity_fac = (-1)**(l+l_+s) #parity of selected modes
-        prefac = np.sqrt((2*l_+1.) * (2*s+1.) * (2*l+1.) \
+        parity_fac = (-1)**(l+l_+self.ss_o) #parity of selected modes
+        prefac = np.sqrt((2*l_+1.) * (2*self.ss_o+1.) * (2*l+1.) \
                     / (4.* np.pi)) * self.wig_red_o(-m_,m_-m,m)
         tstamp('Computed prefac in')
 
@@ -119,68 +98,39 @@ class Hkernels:
         d2Ui_,d2Vi_ = np.gradient(dUi_,r), np.gradient(dVi_,r)
         tstamp('Loaded eigfiles in')
 
-        ##making U,U_,V,V_,dU,dU_,dV,dV_,d2U,d2U_,d2V,d2V_ of same shape
+        #making U,U_,V,V_,dU,dU_,dV,dV_,d2U,d2U_,d2V,d2V_ of same shape
 
         U = np.tile(Ui,(len_s,1))
         V = np.tile(Vi,(len_s,1))
         dU = np.tile(dUi,(len_s,1))
         dV = np.tile(dVi,(len_s,1))
-        tstamp('Tiling initial functions in')
-
-
-        # U_ = np.tile(Ui_,(len_s,len_m,len_m_,1))
-        # V_ = np.tile(Vi_,(len_s,len_m,len_m_,1))
-        # dU_ = np.tile(dUi_,(len_s,len_m,len_m_,1))
-        # dV_ = np.tile(dVi_,(len_s,len_m,len_m_,1))
-        # d2U_ = np.tile(d2Ui_,(len_s,len_m,len_m_,1))
-        # d2V_ = np.tile(d2Vi_,(len_s,len_m,len_m_,1))
-
-        # r = np.tile(r,(len_s,len_m,len_m_,1))
-
         U_ = np.tile(Ui_,(len_s,1))
         V_ = np.tile(Vi_,(len_s,1))
+        tstamp('Tiling initial functions in')
         dU_ = np.tile(dUi_,(len_s,1))
         dV_ = np.tile(dVi_,(len_s,1))
         d2U_ = np.tile(d2Ui_,(len_s,1))
         d2V_ = np.tile(d2Vi_,(len_s,1))
         tstamp('Derivatives tiled in')
         r = np.tile(r,(len_s,1))
-        rf = np.tile(r,(len_m,len_m_,1,1))
-        
+        #        rf = np.tile(r,(len_m_,len_m,1,1))
+
         tstamp()
 
         #B-- EXPRESSION
         Bmm = -r*(self.wig_red(0,-2,2)*om(l,0)*om(l,2)*V*dU_ + self.wig_red(2,-2,0)*om(l_,0)* \
                 om(l_,2)*V_*dU)
         Bmm += self.wig_red(1,-2,1)*om(l_,0)*om(l,0)*(U-V)*(U_ - V_ + r*dV_)
-        tstamp('Computed Bmm without prefac in')
-
-
-        # Bmm = np.tile(Bmm,(len_m,len_m_,1,1))
-
-
-        # Bmm = (((-1)**np.abs(m_))*prefac/(rf**2))[:,:,:,np.newaxis] \
-                #  * Bmm[np.newaxis,:,:]
-        print('Constructed new axes')
         Bmm = (((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
                  * (Bmm/r**2)[np.newaxis,:,:]
-        tstamp('Bmm corrected in')		
         #B-- EXTRA
         Bmm_ = om(l_,0)*(self.wig_red(2,-2,0)*om(l_,2)*U*(V_ - r*dV_) + om(l,0)*V \
                 *(self.wig_red(3,-2,-1)*om(l_,2)*om(l_,3)*V_ + self.wig_red(1,-2,1) \
                 *(-U_ + V_ + om(l_,2)**2 *V_ - r*dV_)))
-
-        # Bmm_ = np.tile(Bmm_,(len_m,len_m_,1,1))
-
-        # Bmm_ = (((-1)**np.abs(m_))*prefac/(rf**2))[:,:,:,np.newaxis] \
-                #  * Bmm_[np.newaxis,:,:]
-
         Bmm_ = (((-1)**np.abs(1+m_))*prefac)[:,:,:,np.newaxis] \
                  * (Bmm_/r**2)[np.newaxis,:,:]
 
         print('Bmm done')
-        print prefac
-
 
         #B0- EXPRESSION
         B0m = self.wig_red(1,-1,0)*om(l_,0)*(U - (om(l,0)**2)*V)*(U_ - V_ + r*dV_)
@@ -188,33 +138,18 @@ class Hkernels:
             + 2*r*self.wig_red(2,-1,-1)*om(l_,2)*V_*dV) + self.wig_red(0,-1,1) \
             *((U-V)*(2*U_ - 2*(om(l_,0)**2)*V_ - r*dU_) + r**2 * dU_*dV))
 
-        # B0m = np.tile(B0m,(len_m,len_m_,1,1))
-        
         B0m = (0.5*((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
                 * (B0m/r**2)[np.newaxis,:,:]
-                
-        
-#        plt.plot(r[0,:],B0m[0,0,0,:])
                 
         #B0- EXTRA
         B0m_ = om(l,0)*V*(self.wig_red(2,-1,-1)*om(l_,0)*om(l_,2)*(U_ - 3*V_ + r*dV_) \
                 + self.wig_red(0,-1,1)*((2+om(l_,0)**2)*U_ - 2*r*dU_ + om(l_,0)**2 \
                 *(-3*V_ + r*dV_)))
         B0m_ += self.wig_red(1,-1,0)*om(l_,0)*U*(U_ - V_ - r*(dU_ - dV_ + r*d2V_))
-        
-
-        # B0m_ = np.tile(B0m_,(len_m,len_m_,1,1))
-
         B0m_ = (0.5*((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
                 * (B0m_/r**2)[np.newaxis,:,:]
                 
         print (np.max(np.abs(prefac))), np.max(np.abs(U)), np.max(np.abs(V)), np.max(np.abs(U_)), np.max(np.abs(V_))
-                
-#        plt.plot(r[0,:],B0m_[0,0,0,:])
-
-        print('B0m done')
-        
-     
 
         #B00 OLD
         B00 = -self.wig_red(0,0,0)*(2*U_ - 2*om(l_,0)**2 * V_ - r*dU_)*(-2*U + 2*om(l,0)**2 *V + \
@@ -267,7 +202,7 @@ class Hkernels:
         B0m = B0m.astype('float64')
         B00 = B00.astype('float64')
         Bpm = Bpm.astype('float64')
-        
+
         #constructing the other two components of the kernel
         Bpp = parity_fac[:,:,:,np.newaxis]*Bmm
         B0p = parity_fac[:,:,:,np.newaxis]*B0m
--- test.py
+++ test.py
@@ -7,13 +7,11 @@ import matplotlib.pyplot as plt
 s = np.array([1])
 m = np.array([2])
 m_ = np.array([2])
-l = 2
+n,l = 1,2
+n_,l_ = n,l
 r = np.loadtxt('r.dat')
-kern = gkerns.Hkernels(l,l,r,700)
+kern = gkerns.Hkernels(n_,l_,n,l,s,-700,-1)
 
-mm, mm_, ss = np.meshgrid(m,m_,s,indexing='ij')
-
-Bmm, B0m,B00, Bpm,_,_ = kern.ret_kerns(l,ss,l,mm,mm_,1,1)
-plt.plot(r[-700:],B0m[0,0,0,:])
-print 'lol'
+Bmm, B0m,B00, Bpm,_,_ = kern.ret_kerns()
+plt.plot(r[-700:-1],B0m[0,0,0,:])
 plt.show()
