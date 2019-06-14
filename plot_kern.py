6a1cbd9a396da9d618f8d40d795827390ef9e8f7
Tuneer Chakraborty <tuneer.ch@gmail.com>
2019-06-14 14:21:23 +0530

    put faster kernel form in get_kernel.py and slower in get_kernel_separate.py

--- plot_kern.py
+++ plot_kern.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import functions as fn
 from os import getcwd
 import get_kernels as gkerns
-import get_kernels_old as gkerns_old
 
 kernclock = timing.stopclock()
 tstamp = kernclock.lap
@@ -15,13 +14,13 @@ start_ind, end_ind = [fn.nearest_index(r, pt) for pt in (r_start, r_end)]
 end_ind = start_ind + 700
 r = r[start_ind:end_ind]
 
-n,l = 1,58
+n,l = 1,60
 n_,l_ = n,l
 m = np.array([2])
 m_ = np.array([2])
-s = np.array([2])
+s = np.array([22])
 
-kern = gkerns_old.Hkernels(n_,l_,m_,n,l,m,s,start_ind,end_ind)
+kern = gkerns.Hkernels(n_,l_,m_,n,l,m,s,start_ind,end_ind)
 Bmm, B0m,B00, Bpm,_,_ = kern.ret_kerns()
 tstamp('kernel calculation time')
 
