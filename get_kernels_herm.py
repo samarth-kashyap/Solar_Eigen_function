90fc5eeb929a39e11e15474dea621ab3387031eb
srijaniiserprinceton <sbdas@princeton.edu>
2019-10-29 21:15:18 -0400

    Modified splitting function. Assymetry still not found.

--- get_kernels_herm.py
+++ get_kernels_herm.py
@@ -52,7 +52,7 @@ class Hkernels:
         wig_vect = np.vectorize(fn.wig,otypes=[float])
         return wig_vect(self.l_,self.ss_i,self.l,m1,m2,m3)
 
-    def ret_kerns(self):
+    def ret_kerns(self,smoothen=False):
         n,l,m,n_,l_,m_ = self.n, self.l, self.mm, self.n_, self.l_, self.mm_
         r_start , r_end = self.r_range
         nl = fn.find_nl(n,l)
@@ -81,36 +81,37 @@ class Hkernels:
         ##########################################################3
         #smoothingR2 = 0.78
         #interpolation params
-        # npts = 3000
-        # r_new = np.linspace(np.amin(self.r),np.amax(self.r),npts)
-        # self.ss_i,__ = np.meshgrid(self.s,r_new, indexing = 'ij')
+        if(smoothen == True):
+            npts = 300
+            r_new = np.linspace(np.amin(self.r),np.amax(self.r),npts)
+            self.ss_i,__ = np.meshgrid(self.s,r_new, indexing = 'ij')
 
-        # Ui,dUi,d2Ui = fn.smooth(self.Ui,self.r,window,order,npts)
-        # Vi,dVi,d2Vi = fn.smooth(self.Vi,self.r,window,order,npts)
+            Ui,dUi,d2Ui = fn.smooth(self.Ui,self.r,window,order,npts)
+            Vi,dVi,d2Vi = fn.smooth(self.Vi,self.r,window,order,npts)
 
-        # Ui_,dUi_,d2Ui_ = fn.smooth(self.Ui_,self.r,window,order,npts)
-        # Vi_,dVi_,d2Vi_ = fn.smooth(self.Vi_,self.r,window,order,npts)
+            Ui_,dUi_,d2Ui_ = fn.smooth(self.Ui_,self.r,window,order,npts)
+            Vi_,dVi_,d2Vi_ = fn.smooth(self.Vi_,self.r,window,order,npts)
 
-        # rho_sm, __, __ = fn.smooth(self.rho,self.r,window,order,npts)
-        # #re-assigning with smoothened variables
-        # r = r_new
-        # rho = rho_sm
+            rho_sm, __, __ = fn.smooth(self.rho,self.r,window,order,npts)
+            #re-assigning with smoothened variables
+            r = r_new
+            rho = rho_sm
 
         
         ###############################################################
         #no smoothing
+        else: 
+            r = self.r
+            rho = self.rho
+            Ui = self.Ui 
+            Vi = self.Vi 
+            Ui_ = self.Ui_ 
+            Vi_ = self.Vi_ 
 
-        r = self.r
-        rho = self.rho
-        Ui = self.Ui 
-        Vi = self.Vi 
-        Ui_ = self.Ui_ 
-        Vi_ = self.Vi_ 
-
-        dUi, dVi = np.gradient(Ui,r), np.gradient(Vi,r)
-        d2Ui,d2Vi = np.gradient(dUi,r), np.gradient(dVi,r)
-        dUi_, dVi_ = np.gradient(Ui_,r), np.gradient(Vi_,r)
-        d2Ui_,d2Vi_ = np.gradient(dUi_,r), np.gradient(dVi_,r)
+            dUi, dVi = np.gradient(Ui,r), np.gradient(Vi,r)
+            d2Ui,d2Vi = np.gradient(dUi,r), np.gradient(dVi,r)
+            dUi_, dVi_ = np.gradient(Ui_,r), np.gradient(Vi_,r)
+            d2Ui_,d2Vi_ = np.gradient(dUi_,r), np.gradient(dVi_,r)
         # tstamp('load eigfiles')
 
         #################################################################3
@@ -366,7 +367,157 @@ class Hkernels:
         else:
             return Bmm,B0m,B00,Bpm,Bp0,Bpp
         
+    def ret_splittingfunction_terms(self,smoothen = False):
+        n,l,n_,l_ = self.n, self.l, self.n_, self.l_
+        r_start , r_end = self.r_range
+        nl = fn.find_nl(n,l)
+        nl_ = fn.find_nl(n_,l_)
 
+        len_s,__ = np.shape(self.ss_i)
+
+        #Savitsky golay filter for smoothening
+        window = 45  #must be odd
+        order = 3
+
+        if(nl == None or nl_ == None):
+            print("Mode not found. Exiting."); exit()
+
+
+        tstamp()
+        om = np.vectorize(fn.omega,otypes=[float])
+        wig_val = np.vectorize(fn.wig,otypes=[float])
+        parity_fac = (-1)**(l+l_+self.s) #parity of selected modes
+        prefac = 1./(4.* np.pi)/wig_val(l_,self.s,l,0,0,0)
+        prefac[np.isinf(prefac)] = 0.0  #changing infinities to zeros. Splitting functions will be zeros Happens for l_+s+l=odd
+
+        # tstamp('prefac computation')
+
+        #EIGENFUCNTION DERIVATIVES
+
+        ##########################################################3
+        #smoothingR2 = 0.78
+        #interpolation params
+        if(smoothen == True):
+            npts = 300
+            r_new = np.linspace(np.amin(self.r),np.amax(self.r),npts)
+            self.ss_i,__ = np.meshgrid(self.s,r_new, indexing = 'ij')
+
+            Ui,dUi,d2Ui = fn.smooth(self.Ui,self.r,window,order,npts)
+            Vi,dVi,d2Vi = fn.smooth(self.Vi,self.r,window,order,npts)
+
+            Ui_,dUi_,d2Ui_ = fn.smooth(self.Ui_,self.r,window,order,npts)
+            Vi_,dVi_,d2Vi_ = fn.smooth(self.Vi_,self.r,window,order,npts)
+
+            rho_sm, __, __ = fn.smooth(self.rho,self.r,window,order,npts)
+            #re-assigning with smoothened variables
+            r = r_new
+            rho = rho_sm
+
+        
+        ###############################################################
+        #no smoothing
+        else: 
+            r = self.r
+            rho = self.rho
+            Ui = self.Ui 
+            Vi = self.Vi 
+            Ui_ = self.Ui_ 
+            Vi_ = self.Vi_ 
+
+            dUi, dVi = np.gradient(Ui,r), np.gradient(Vi,r)
+            d2Ui,d2Vi = np.gradient(dUi,r), np.gradient(dVi,r)
+            dUi_, dVi_ = np.gradient(Ui_,r), np.gradient(Vi_,r)
+            d2Ui_,d2Vi_ = np.gradient(dUi_,r), np.gradient(dVi_,r)
+        # tstamp('load eigfiles')
+
+        #################################################################3
+
+        #making U,U_,V,V_,dU,dU_,dV,dV_,d2U,d2U_,d2V,d2V_ of same shape
+
+        U = np.tile(Ui,(len_s,1))
+        V = np.tile(Vi,(len_s,1))
+        dU = np.tile(dUi,(len_s,1))
+        dV = np.tile(dVi,(len_s,1))
+        d2U = np.tile(d2Ui,(len_s,1))
+        d2V = np.tile(d2Vi,(len_s,1))
+        U_ = np.tile(Ui_,(len_s,1))
+        V_ = np.tile(Vi_,(len_s,1))
+        dU_ = np.tile(dUi_,(len_s,1))
+        dV_ = np.tile(dVi_,(len_s,1))
+        d2U_ = np.tile(d2Ui_,(len_s,1))
+        d2V_ = np.tile(d2Vi_,(len_s,1))
+        r = np.tile(r,(len_s,1))
+
+        tstamp()
+
+        # Initializing the omegas which will be used very often. Might cut down on some computational time
+
+        om0 = om(l,0)
+        om0_ = om(l_,0)
+        om2 = om(l,2)
+        om2_ = om(l_,2)
+        om3 = om(l,3)
+        om3_ = om(l_,3)
+
+        #B-- EXPRESSION
+        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3*U - 2*om0**2*V + 3*r*dU) - r*U*dV_)
+        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3*U_ - 2*om0_**2*V_ + 3*r*dU_) - r*U_*dV)
+        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3*U*V_ - 2*om0_**2*V_*V + om2_**2*V_*V + r*V*dU_ - r*U*dV_ - U_*U)
+        Bmm += self.wig_red(1,-2,1)*om0*om0_*(3*U_*V - 2*om0**2*V*V_ + om2**2*V*V_ + r*V_*dU - r*U_*dV - U*U_)
+        Bmm += self.wig_red(3,-2,-1)*om2_*om3_*om0*om0_*V_*V
+        Bmm += self.wig_red(-1,-2,3)*om2*om3*om0_*om0*V*V_
+        
+        # np.save('prefac.npy',prefac)
+
+        Bmm = 0.5*(((-1)**np.abs(1))*prefac)[:,np.newaxis] * (Bmm/r**2)
+    
+
+        #tstamp('Bmm done')
+
+        #B0- EXPRESSION
+        B0m = self.wig_red(1,-1,0)*om0_*(4*om0**2*V_*V + 8*U*U_ - 5*om0**2*U_*V - 3*r*om0**2*V*dV_ + 2*r**2*dU*dV_ - r*om0**2*V_*dV + \
+                r**2*V_*d2U + (-6.-2.*om0_**2 + om0**2)*U*V_ + r*U*(4*dV_ - r*d2V_))
+        B0m += self.wig_red(0,-1,1)*om0*(4*om0_**2*V*V_ + 8*U_*U - 5*om0_**2*U*V_ - 3*r*om0_**2*V_*dV + 2*r**2*dU_*dV - r*om0**2*V*dV_ + \
+                r**2*V*d2U_ + (-6. -2.*om0**2 + om0_**2)*U_*V + r*U_*(4*dV - r*d2V))
+        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*U_ - 4*V*V_ + 3*r*dV_*V + r*V_*dV)
+        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*U - 4*V_*V + 3*r*dV*V_ + r*V*dV_)
+
+        B0m = (0.25*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (B0m/r**2)
+
+        #tstamp('B0m done')
+        
+#        print(np.shape(self.wig_red(-1,-0,1)))
+#        exit()
+
+        #B00 EXPRESSION
+        B00 = (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(2*(-1. + om0_**2)*V_*V - V_*(3*r*dU - 4*r*dV) - 2*r*U*dV_ - 2*r**2*dV_*dV)
+        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(2*(-1. + om0**2)*V*V_ - V*(3*r*dU_ - 4*r*dV_) - 2*r*U_*dV - 2*r**2*dV*dV_)
+        B00 += self.wig_red(0,0,0)*(-26*om0_**2*U*V_ + 12*om0_**2*om0**2*V_*V + 2*om0_**2*r*U*dV_ - 2*r*dU_*U + r**2*dU_*dU + r**2*U*d2U_ + 14*U*U_)
+        B00 += self.wig_red(0,0,0)*(-26*om0**2*U_*V + 12*om0**2*om0_**2*V*V_ + 2*om0**2*r*U_*dV - 2*r*dU*U_ + r**2*dU*dU_ + r**2*U_*d2U + 14*U_*U)
+
+        B00 = (0.25*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (B00/r**2)
+        #tstamp('B00 done')
+
+        #B+- EXPRESSION
+        Bpm = self.wig_red(0,0,0)*(4*om0_**2*U*V_ - 4*om0_**2*om0**2*V_*V - 10*r*U*dU_ + 6*r*om0**2*V*dU_ - 3*r**2*dU_*dU + 4*r*om0_**2*U*dV_ - r**2*U*d2U_)
+        Bpm += self.wig_red(0,0,0)*(4*om0**2*U_*V - 4*om0**2*om0_**2*V*V_ - 10*r*U_*dU + 6*r*om0_**2*V_*dU - 3*r**2*dU*dU_ + 4*r*om0**2*U_*dV - r**2*U_*d2U)
+        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0_*om0*om2_*om2*V_*V)
+        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0*om0_*om2*om2_*V*V_)
+        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(V_*U - 2*om0_**2*V_*V + r*U*dV_ + U_*U)
+        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(V*U_ - 2*om0**2*V*V_ + r*U_*dV + U*U_)
+
+
+        Bpm = (0.25*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (Bpm/r**2)
+
+        #tstamp('Bpm done')
+
+        # print(prefac.dtype,parity_fac.dtype,Bmm.dtype,B0m.dtype,B00.dtype,Bpm.dtype)
+
+        #constructing the other two components of the kernel
+        Bpp = parity_fac[:,np.newaxis]*Bmm
+        Bp0 = parity_fac[:,np.newaxis]*B0m
+
+        return Bmm,B0m,B00,Bpm,Bp0,Bpp
         
     def Tkern(self,s):   
         n,l,n_,l_ = self.n, self.l, self.n_, self.l_
--- splitting_function.py
+++ splitting_function.py
@@ -5,12 +5,28 @@ import get_kernels_herm as gkerns
 import functions as fn
 import scipy.integrate
 import matplotlib.pyplot as plt 
+import random
 plt.ion()
 
-n,l = 2,4
-n_,l_ = 2,4
+#Modes whose splitting function is to be computed
+n,l = 10,3
+n_,l_ = 9,4
 
-def uniform_Hprofile(r):
+r = np.loadtxt('r.dat')
+r_start, r_end = 0.,1.
+start_ind, end_ind = [fn.nearest_index(r, pt) for pt in (r_start, r_end)]
+r = r[start_ind:end_ind]
+
+#Lorentz stress components' order
+s = np.array([1,2,3])
+smax = np.amax(s)
+s_H = 2*smax + 1         #triangle law 0 <= s_H <= 2s;    s_H for H and s for B
+
+om = np.vectorize(fn.omega,otypes=[float])
+wig_val = np.vectorize(fn.wig,otypes=[float])
+
+#B magnitude profile as a function of radius which is simply squared to get H magnitude for different components
+def B0_st_profile(r):
     beta = 1e-4/r**3  #10G on surface
     #1e5 Gauss at tachocline
     beta += np.exp(-0.5*((r-0.7)/0.01)**2)
@@ -18,65 +34,146 @@ def uniform_Hprofile(r):
     beta += 100*np.exp(-0.5*(r/0.1)**2)
     return beta
 
+def ret_Bcomps(s,r):
+    B0_st = B0_st_profile(r)
+    #Implementing the solenoidal condition
+    gradr_B0st = (np.gradient(r**2*B0_st,r)/r)[np.newaxis,:]/om(s,0)[:,np.newaxis]      #shape s x r
+    rand_factors = np.zeros((smax,2*smax+1))
+    one_factors = np.zeros((smax,2*smax+1))
+    for s0 in s:
+        rand_factors[s0-1,smax-s0:smax+s0+1] = np.array([random.random() for i in range(2*s0+1)]) #shape s x t
+        one_factors[s0-1,smax-s0:smax+s0+1] = np.array([1.0 for i in range(2*s0+1)]) #shape s x t
 
-r = np.loadtxt('r.dat')
-r_start = 0.68
-r_end = 1.0
-start_ind = fn.nearest_index(r,r_start)
-end_ind = fn.nearest_index(r,r_end)
-r = r[start_ind:end_ind+1]
+    B0_st = B0_st[np.newaxis,:]*rand_factors[:,:,np.newaxis]
+    gradr_B0st = gradr_B0st[:,np.newaxis,:]*rand_factors[:,:,np.newaxis]
+    Bp_st = rand_factors[:,:,np.newaxis]*gradr_B0st    #shape s x t x r
+    Bm_st = gradr_B0st - Bp_st
 
-B_mag = uniform_Hprofile(r)
-H_mag = B_mag**2
+    B_st_total = np.zeros((3,smax,2*smax+1,len(r)))
+    B_st_total[0] = Bm_st
+    B_st_total[1] = B0_st
+    B_st_total[2] = Bp_st
+    return B_st_total
 
-#Specifying Lorentz stress component
-hfac = np.array([1,1,1,1,1,1])
+def ret_Hcomps(mu,nu,s0,t0):
+    #computing the brute force way
+#     H_st = np.zeros(len(r))
+#     for s1 in s:
+#         for s2 in s:
+#             for t1 in range(-s1,s1+1):
+#                 for t2 in range(-s2,s2+1):
+#                 #     print(s1,s2,t1)
+#                     H_st_temp = B_st[mu,s1-1,t1+smax,:]*B_st[nu,s2-1,t2+smax,:]
+#                     H_st_temp = H_st_temp * (-1)**(np.abs(mu+nu+t0)) * np.sqrt((2*s0+1.)*(2*s1+1.)*(2*s2+1.)/(4.* np.pi))
+#                     H_st_temp = H_st_temp * wig_val(s0,s1,s2,-t0,t1,t2) * wig_val(s0,s1,s2,-(mu+nu),mu,nu)
+#                     H_st += H_st_temp
 
-#distributing the components
-hmm = hfac[0]*H_mag
-h0m = hfac[1]*H_mag
-h00 = hfac[2]*H_mag
-hpm = hfac[3]*H_mag
-hp0 = hfac[4]*H_mag
-hpp = hfac[5]*H_mag
+    #computing using numpy array handling
 
-#Lorentz stress components' order
-s = np.array([0,1,2,3,4,5,6])
-t = 4
+    H_st_mat = B_st[mu,:,:,np.newaxis,np.newaxis,:]*B_st[nu,np.newaxis,np.newaxis,:,:,:]
+    t = np.arange(-smax,smax+1)
+    ss1,tt1,ss2,tt2 = np.meshgrid(s,t,s,t,indexing='ij')
 
-#The specific angular degree to plot
-s0 = 4
+    prefactor = (-1)**(np.abs(mu+nu+t0)) * np.sqrt((2*s0+1.)*(2*ss1+1.)*(2*ss2+1.)/(4.* np.pi))
+    prefactor *= wig_val(s0,ss1,ss2,-t0,tt1,tt2) * wig_val(s0,ss1,ss2,-(mu+nu),mu,nu)
+
+    H_st_mat = H_st_mat * prefactor[:,:,:,:,np.newaxis]
+    H_st_mat = np.sum(H_st_mat,axis=(0,1,2,3))
+
+    return H_st_mat
+
+
+#getting the kernel components
+
 
 m = np.arange(-l,l+1,1)    #-l<=m<=l
 m_ = np.arange(-l_,l_+1,1)  #-l_<=m<=l_ 
 
-kern = gkerns.Hkernels(n_,l_,m_,n,l,m,s,r,False)
-Bmm,B0m,B00,Bpm,Bp0,Bpp = kern.ret_kerns()
-#sys.exit()
+s_H_arr = np.arange(s_H)
 
-#find integrand by summing all component
-Lambda_sr = hpp[np.newaxis,:]*Bpp + h00[np.newaxis,:]*B00 + hmm[np.newaxis,:]*Bmm \
-        + 2*hpm[np.newaxis,:]*Bpm + 2*h0m[np.newaxis,:]*B0m + 2*hp0[np.newaxis,:]*Bp0
+kern = gkerns.Hkernels(n_,l_,m_,n,l,m,s_H_arr,r,False)
+Bmm,B0m,B00,Bpm,Bp0,Bpp = kern.ret_splittingfunction_terms(smoothen=True)
 
-#summing over s before carrying out radial integral
-Lambda_r = Lambda_sr[m[0]+t,m[0],s0,:]
+npts = 300   #check the npts in get_kernels
+r_new = np.linspace(np.amin(r),np.amax(r),npts)
+r = r_new
 
-#radial integral
-Lambda = scipy.integrate.trapz(Lambda_r*(r**2),x=r,axis=0)
+B_st = ret_Bcomps(s,r)     #computes all components of all st of 
 
+#finding the H-components
+Hmm_str = np.zeros((s_H,2*s_H+1,len(r)))
+H0m_str = np.zeros((s_H,2*s_H+1,len(r)))
+H00_str = np.zeros((s_H,2*s_H+1,len(r)))
+Hpm_str = np.zeros((s_H,2*s_H+1,len(r)))
+H0p_str = np.zeros((s_H,2*s_H+1,len(r)))
+Hpp_str = np.zeros((s_H,2*s_H+1,len(r)))
 
+print('Here')
+
+for s0 in range(s_H):    
+#     for t0 in range(-s0,0):
+#             Hmm_str[s0,t0+s_H,:]  = ret_Hcomps(-1,-1,s0,t0)
+#             H0m_str[s0,t0+s_H,:]  = ret_Hcomps(0,-1,s0,t0)
+#             H00_str[s0,t0+s_H,:]  = ret_Hcomps(0,0,s0,t0)
+#             Hpm_str[s0,t0+s_H,:]  = ret_Hcomps(1,-1,s0,t0)
+
+#             #Imposing the realness of H along with its symmetry in mu,nu
+#             Hpp_str[s0,-t0+s_H,:] = (-1)**(np.abs(t0)) * np.conj(Hmm_str[s0,t0+s_H,:])
+#             H0p_str[s0,-t0+s_H,:] = (-1)**(np.abs(t0)) * np.conj(H0m_str[s0,t0+s_H,:])
+
+#             H00_str[s0,-t0+s_H,:] = (-1)**(np.abs(t0)) * np.conj(H00_str[s0,t0+s_H,:])
+#             Hpm_str[s0,-t0+s_H,:] = (-1)**(np.abs(t0)) * np.conj(Hpm_str[s0,t0+s_H,:])
+
+#     for t0 in range(0,s0+1):
+#             Hmm_str[s0,t0+s_H,:]  = ret_Hcomps(-1,-1,s0,t0)
+#             H0m_str[s0,t0+s_H,:]  = ret_Hcomps(0,-1,s0,t0)
+
+#             #Imposing the realness of H along with its symmetry in mu,nu
+#             Hpp_str[s0,-t0+s_H,:] = (-1)**(np.abs(t0)) * np.conj(Hmm_str[s0,t0+s_H,:])
+#             H0p_str[s0,-t0+s_H,:] = (-1)**(np.abs(t0)) * np.conj(H0m_str[s0,t0+s_H,:])
+
+    for t0 in range(-s0,s0+1):
+            Hmm_str[s0,t0+s_H,:] = ret_Hcomps(-1,-1,s0,t0)
+            H0m_str[s0,t0+s_H,:] = ret_Hcomps(0,-1,s0,t0)
+            H00_str[s0,t0+s_H,:] = ret_Hcomps(0,0,s0,t0)
+            Hpm_str[s0,t0+s_H,:] = ret_Hcomps(1,-1,s0,t0)
+            Hpp_str[s0,t0+s_H,:] = ret_Hcomps(1,1,s0,t0)
+            H0p_str[s0,t0+s_H,:] = ret_Hcomps(0,1,s0,t0)
+
+            
+    print(s0,t0)
 
 #plotting on Mollweide projection
 theta = np.linspace(-np.pi/2,np.pi/2,360)
 phi = np.linspace(-np.pi,np.pi,720)
 
-tt,pp = np.meshgrid(theta,phi,indexing='ij')
+thth,phph = np.meshgrid(theta,phi,indexing='ij')
 
-z = Lambda*sp.sph_harm(s0,t,pp,tt)
+#find integrand by summing all component
+Lambda_str = Hpp_str*Bpp[:,np.newaxis,:] + H00_str*B00[:,np.newaxis,:] + Hmm_str*Bmm[:,np.newaxis,:] \
+        + 2*Hpm_str*Bpm[:,np.newaxis,:] + 2*H0m_str*B0m[:,np.newaxis,:] + 2*H0p_str*Bp0[:,np.newaxis,:]
+
+#radial integral
+Lambda_st = scipy.integrate.trapz(Lambda_str*(r**2),x=r,axis=2)
+
+t = np.arange(-s_H,s_H+1)
+ss, tt = np.meshgrid(s_H_arr,t,indexing='ij')
+
+z = np.zeros(np.shape(thth),dtype='complex128')
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='mollweide')
 
-im = ax.pcolormesh(pp,tt,np.real(z), cmap=plt.cm.jet)
+for s0 in s_H_arr:
+    for t0 in range(-s0,s0+1):
+        z += Lambda_st[s0,t0+s_H]*sp.sph_harm(t0,s0,phph,thth)
+        
+im = ax.pcolormesh(phph,thth,np.real(z), cmap=plt.cm.jet)
+#  plt.colorbar()  
 
-plt.show()
+
+
+
+
+
+
