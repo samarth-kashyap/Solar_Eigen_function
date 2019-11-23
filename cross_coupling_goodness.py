Major change: Kernel expressions corrected in get_kernels_herm.py and minor change: write_w.py does not subtract surface rotation in Differential rotation perturbation.

cross_coupling_goodness.py
@@ -37,7 +37,7 @@ f_window = 50.0 #in muHz
 #tolerance of offset of QDPT wet DPT beyond which cross-coupling is important
 tol_percent = 5.0   #in percent. tol = 5.0 means 5% tolerance or QPDT rms has to be within 95% of DPT rms.
 
-s = np.array([0,1,2,3,4,5,6])
+s = np.array([0,1,2])
 t = np.arange(-np.amax(s),np.amax(s)+1)
 # t = np.array([0])
 # n0 = 6  #choosing the branch
@@ -53,14 +53,16 @@ s_max_H = np.max(s)
 smoothen = False
 
 #4 components of munu as we ar clubbing together (-- with ++) and (0- with 0+)
-qdpt_contrib_rel = np.zeros((30,300,4))
+# qdpt_contrib_rel = np.zeros((30,300,4))
+qdpt_dev_nhz = np.zeros((30,300,4))
 # qdpt_contrib = np.zeros((4,300))
 
-for n0 in range(0,9):
+for n0 in range(0,20):
     nl_list_n0 = nl_all[nl_all[:,0]==n0]
     l_n0 = nl_list_n0[:,1]
 
-    l_min = int(np.amin(l_n0) + np.amax(s))
+    # l_min = int(np.amin(l_n0) + np.amax(s))
+    l_min = int(np.amin(l_n0))
     #running over all the central modes whose frequency shifts we want to find
     for l0 in range(l_min,l_max+1):
         # nl_list = np.zeros((2*np.amax(s)+1,2))  #modes to be considered for a certain central mode
@@ -177,58 +179,100 @@ for n0 in range(0,9):
             # plt.savefig('./Cross_coupling_goodness/l_%i_munu_%i'%(l0,munu))
             # plt.close()
 
+            #in order to isolate the central mode from the QDPT sorted eigenvalues
 
+            omega_nl_sorted = np.sort(omega_nl)
+            sort_ind = np.argsort(omega_nl)
+            l0_location = np.argmin(np.abs(omega_nl_sorted - omega_nl0))
+            nl_sorted = nl_list[sort_ind]
+            l_local_start = 2*np.sum(nl_sorted[:l0_location,1]) + l0_location
+            l_local_end = l_local_start + 2*l0+1
+
+            ell = np.arange(l_local_start,l_local_end)
 
             #Checking if we should already discard some modes which come too close or overlap.
-            if(freq_jump <= nearest_omega_jump/4 or cent_mode_SD/freq_jump > 0.3):
-                qdpt_contrib_rel[n0,l0,munu] = 100.0    #set a high value
-                title ='Unclean'    #Marking them unclean to label the plots
+            # if(freq_jump <= nearest_omega_jump/4 or cent_mode_SD/freq_jump > 0.3):
+            #     # qdpt_contrib_rel[n0,l0,munu] = 100.0    #set a high value
+            #     qdpt_dev_nhz[n0,l0,munu] = 2000.0
+            #     title ='Unclean'    #Marking them unclean to label the plots
 
-                plt.figure()
-                plt.plot(np.sort(np.abs(eig_vals_qdpt/(2*omega_ref0))),'.')
-                plt.plot(np.sort(np.abs(omega_nl_arr-omega_ref0))) 
-                plt.savefig('./Coupled_modes/%i_%i_%i.png'%(n0,l0,munu))
-                plt.close()
+            #     plt.figure()
+            #     # plt.plot(np.sort(np.abs(eig_vals_qdpt/(2*omega_ref0))),'.')
+            #     # plt.plot(np.sort(np.abs(omega_nl_arr-omega_ref0))) 
 
-                continue    #abandon further analysis
+            #     plt.plot(ell,f_qdpt[l_local_start:l_local_end],'.',label='QDPT')
+            #     plt.plot(ell,np.sort(omega_nl_arr)[l_local_start:l_local_end]*OM*1e6,'.-',label='0')
+            #     plt.plot(ell,f_dpt,'.--',label='DPT')
+
+            #     plt.savefig('./Coupled_modes/BAD_%i_%i_%i.png'%(n0,l0,munu))
+            #     plt.close()
+
+            #     continue    #abandon further analysis
+
+            # else:
+
+            #     plt.figure()
+            #     # plt.plot(np.sort(np.abs(eig_vals_qdpt/(2*omega_ref0))),'.')
+            #     # plt.plot(np.sort(np.abs(omega_nl_arr-omega_ref0))) 
+
+            #     plt.plot(ell,f_qdpt[l_local_start:l_local_end],'.',label='QDPT')
+            #     plt.plot(ell,np.sort(omega_nl_arr)[l_local_start:l_local_end]*OM*1e6,'.-',label='0')
+            #     plt.plot(ell,f_dpt,'.--',label='DPT')
+
+            #     plt.savefig('./Coupled_modes/GOOD_%i_%i_%i.png'%(n0,l0,munu))
+            #     plt.close()
                 
 
             #######################
             #Comparing QDPT and DPT
 
-            domega_QDPT = np.linalg.norm(np.sort(np.abs(eig_vals_qdpt))[:2*l0+1])
-            domega_DPT = np.linalg.norm(eig_vals_dpt)
+            # domega_QDPT = np.linalg.norm(np.sort(np.abs(eig_vals_qdpt))[:2*l0+1])
+            # domega_DPT = np.linalg.norm(eig_vals_dpt)
 
-            rel_offset_percent = np.abs((domega_QDPT-domega_DPT)/domega_DPT) * 100.0
+            # rel_offset_percent = np.abs((domega_QDPT-domega_DPT)/domega_DPT) * 100.0
+
+
 
             # if(rel_offset_percent <= tol_percent): 
-            qdpt_contrib_rel[n0,l0,munu] = rel_offset_percent  
+            # qdpt_contrib_rel[n0,l0,munu] = rel_offset_percent  
+            
+
+            freq_diff_nHz = 1000*(f_qdpt[l_local_start:l_local_end] - f_dpt)    #frequency offset in nHz
+
+            qdpt_dev_nhz[n0,l0,munu] = np.average(np.abs(freq_diff_nHz))
+
+
 
                 
 
-
+qdpt_dev_nhz = np.ma.masked_greater(qdpt_dev_nhz,100)
 
 l0 = 30
 l = np.arange(0,l0)
 
 for munu in range(4): 
     plt.figure()
-    for n0 in range(9):
+    for n0 in range(20):
         nl_list = nl_all[nl_all[:,0]==n0][:l0]
         nl_list = nl_list.astype('int64')
 
         omega_nl = np.array([omega_list[fn.find_nl(mode[0], mode[1])] for mode in nl_list]) #important to have nl_list as integer type
 
         vmin = 0
-        vmax = np.amax(qdpt_contrib_rel[:,:l0, munu])
-        plt.scatter(l,omega_nl*OM*1e6,c=qdpt_contrib_rel[n0,:l0, munu],vmin=vmin,vmax=90)
+        # vmax = np.amax(qdpt_contrib_rel[:,:l0, munu])
+        vmax = np.amax(qdpt_dev_nhz[:,:l0, munu])
+        # plt.scatter(l,omega_nl*OM*1e6,c=qdpt_contrib_rel[n0,:l0, munu],vmin=vmin,vmax=90)
+        plt.scatter(l,omega_nl*OM*1e6,c=qdpt_dev_nhz[n0,:l0, munu],vmin=vmin,vmax=vmax)
+
     plt.colorbar()
     if(munu==0): plt.title('$\mathcal{B}^{--}$')
     elif(munu==1): plt.title('$\mathcal{B}^{0-}$')
     elif(munu==2): plt.title('$\mathcal{B}^{00}$')
     else: plt.title('$\mathcal{B}^{+-}$')
 
+    plt.ylim([480,4300])
+
     plt.xlabel('$\ell$')
     plt.ylabel('frequency (in $\mu$ Hz)')
-    plt.savefig('Cross_coupling_goodness/Bmunu_%i.png'%munu)
+    plt.savefig('Cross_coupling_goodness/Bmunu_avg_%i.png'%munu)
 

get_kernels_herm.py
@@ -144,12 +144,12 @@ class Hkernels:
         om3_ = om(l_,3)
 
         #B-- EXPRESSION
-        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3*U - 2*om0**2*V + 3*r*dU) - r*U*dV_)
-        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3*U_ - 2*om0_**2*V_ + 3*r*dU_) - r*U_*dV)
-        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3*U*V_ - 2*om0_**2*V_*V + om2_**2*V_*V + r*V*dU_ - r*U*dV_ - U_*U)
-        Bmm += self.wig_red(1,-2,1)*om0*om0_*(3*U_*V - 2*om0**2*V*V_ + om2**2*V*V_ + r*V_*dU - r*U_*dV - U*U_)
-        Bmm += self.wig_red(3,-2,-1)*om2_*om3_*om0*om0_*V_*V
-        Bmm += self.wig_red(-1,-2,3)*om2*om3*om0_*om0*V*V_
+        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3.*U-2.*om0**2 *V + 3.*r*dU) - r*U*dV_)
+        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3.*U_-2.*om0_**2 *V_ + 3.*r*dU_) - r*U_*dV)
+        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3.*U*V_+3.*U_*V-2.*om0_**2 *V_*V - 2.*om0**2 *V_*V \
+                + om2_**2*V_*V + om2**2 *V_*V + r*V*dU_ + r*V_*dU - r*U*dV_ - r*U_*dV - 2.*U_*U)
+        Bmm += self.wig_red(3,-2,-1)*om0*om0_*om2_*om3_*V_*V
+        Bmm += self.wig_red(-1,-2,3)*om0_*om0*om2*om3*V_*V                
         
         # np.save('prefac.npy',prefac)
 
@@ -160,12 +160,12 @@ class Hkernels:
         #tstamp('Bmm done')
 
         #B0- EXPRESSION
-        B0m = self.wig_red(1,-1,0)*om0_*(4*om0**2*V_*V + 8*U*U_ - 5*om0**2*U_*V - 3*r*om0**2*V*dV_ + 2*r**2*dU*dV_ - r*om0**2*V_*dV + \
-                r**2*V_*d2U + (-6.-2.*om0_**2 + om0**2)*U*V_ + r*U*(4*dV_ - r*d2V_))
-        B0m += self.wig_red(0,-1,1)*om0*(4*om0_**2*V*V_ + 8*U_*U - 5*om0_**2*U*V_ - 3*r*om0_**2*V_*dV + 2*r**2*dU_*dV - r*om0_**2*V*dV_ + \
-                r**2*V*d2U_ + (-6. -2.*om0**2 + om0_**2)*U_*V + r*U_*(4*dV - r*d2V))
-        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*U_ - 4*V*V_ + 3*r*dV_*V + r*V_*dV)
-        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*U - 4*V_*V + 3*r*dV*V_ + r*V*dV_)
+        B0m = self.wig_red(1,-1,0)*om0_*(4.*om0**2 *V_*V + U_*(8.*U-5.*om0**2* V) - 3.*r*om0**2*V*dV_ \
+                + 2*r**2*dU*dV_ - r*om0**2 *V_*dV + r**2 *V_*d2U + U*((-6.-2.*om0_**2+om0**2)*V_ + r*(4.*dV_-r*d2V_)))
+        B0m += self.wig_red(0,-1,1)*om0*(4.*om0_**2 *V*V_ + U*(8.*U_-5.*om0_**2 *V_) - 3.*r*om0_**2 *V_*dV \
+                + 2*r**2*dU_*dV - r*om0_**2 *V*dV_ + r**2 *V*d2U_ + U_*((-6.-2.*om0**2+om0_**2)*V + r*(4.*dV-r*d2V)))
+        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*(U_-4.*V_+3.*r*dV_) + r*V_*dV)
+        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*(U-4.*V+3.*r*dV) + r*V*dV_)
 
         B0m = (0.25*((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
                 * (B0m/r**2)[np.newaxis,:,:]
@@ -176,22 +176,20 @@ class Hkernels:
 #        exit()
 
         #B00 EXPRESSION
-        B00 = (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(2*(-1. + om0_**2)*V_*V - V_*(3*r*dU - 4*r*dV) - 2*r*U*dV_ - 2*r**2*dV_*dV)
-        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(2*(-1. + om0**2)*V*V_ - V*(3*r*dU_ - 4*r*dV_) - 2*r*U_*dV - 2*r**2*dV*dV_)
-        B00 += self.wig_red(0,0,0)*(-26*om0_**2*U*V_ + 12*om0_**2*om0**2*V_*V + 2*om0_**2*r*U*dV_ - 2*r*dU_*U + r**2*dU_*dU + r**2*U*d2U_ + 14*U*U_)
-        B00 += self.wig_red(0,0,0)*(-26*om0**2*U_*V + 12*om0**2*om0_**2*V*V_ + 2*om0**2*r*U_*dV - 2*r*dU*U_ + r**2*dU*dU_ + r**2*U_*d2U + 14*U_*U)
+        B00 = self.wig_red(0,0,0)*2.*(-2.*r*U*dU_ - 2.*r*U_*dU + om0**2*r*V*dU_ + om0_**2*r*V_*dU - 5.*om0_**2*V_*U \
+                - 5.*om0**2*V*U_ + 4.*om0**2*om0_**2*V_*V + om0_**2*r*U*dV_ + om0**2*r*U_*dV + 6.*U_*U)
+        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*(-om0_*om0)*(-U_*V-U*V_+2.*V_*V+r*V*dU_\
+                +r*V_*dU-2.*r*V*dV_-2*r*V_*dV+r*U*dV_+r*U_*dV+2*r**2 *dV_*dV)
 
-        B00 = (0.25*((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
+        B00 = (0.5*((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
                 * (B00/r**2)[np.newaxis,:,:]
         #tstamp('B00 done')
 
         #B+- EXPRESSION
-        Bpm = self.wig_red(0,0,0)*(4*om0_**2*U*V_ - 4*om0_**2*om0**2*V_*V - 10*r*U*dU_ + 6*r*om0**2*V*dU_ - 3*r**2*dU_*dU + 4*r*om0_**2*U*dV_ - r**2*U*d2U_)
-        Bpm += self.wig_red(0,0,0)*(4*om0**2*U_*V - 4*om0**2*om0_**2*V*V_ - 10*r*U_*dU + 6*r*om0_**2*V_*dU - 3*r**2*dU*dU_ + 4*r*om0**2*U_*dV - r**2*U_*d2U)
-        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0_*om0*om2_*om2*V_*V)
-        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0*om0_*om2*om2_*V*V_)
-        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(V_*U - 2*om0_**2*V_*V + r*U*dV_ + U_*U)
-        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(V*U_ - 2*om0**2*V*V_ + r*U_*dV + U*U_)
+        Bpm = self.wig_red(0,0,0)*2.*(-2.*r*dU_*U-2.*r*dU*U_+om0**2*r*dU_*V+om0_**2*r*dU*V_-2.*r**2*dU_*dU \
+                -om0_**2*U*V_-om0**2*U_*V+om0_**2*r*U*dV_+om0**2*r*U_*dV + 2.*U_*U)
+        Bpm += (self.wig_red(-2,0,2)+self.wig_red(2,0,-2))*(-4.*om0*om0_*om2*om2_*V_*V)
+        Bpm += (self.wig_red(-1,0,1)+self.wig_red(1,0,-1))*(om0*om0_)*(-r*V*dU_-r*V_*dU-V_*U-V*U_+r*U*dV_+r*U_*dV+2.*U_*U)
 
 
         Bpm = (0.25*((-1)**np.abs(m_))*prefac)[:,:,:,np.newaxis] \
@@ -309,24 +307,24 @@ class Hkernels:
         om3_ = om(l_,3)
 
         #B-- EXPRESSION
-        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3*U - 2*om0**2*V + 3*r*dU) - r*U*dV_)
-        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3*U_ - 2*om0_**2*V_ + 3*r*dU_) - r*U_*dV)
-        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3*U*V_ - 2*om0_**2*V_*V + om2_**2*V_*V + r*V*dU_ - r*U*dV_ - U_*U)
-        Bmm += self.wig_red(1,-2,1)*om0*om0_*(3*U_*V - 2*om0**2*V*V_ + om2**2*V*V_ + r*V_*dU - r*U_*dV - U*U_)
-        Bmm += self.wig_red(3,-2,-1)*om2_*om3_*om0*om0_*V_*V
-        Bmm += self.wig_red(-1,-2,3)*om2*om3*om0_*om0*V*V_
+        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3.*U-2.*om0**2 *V + 3.*r*dU) - r*U*dV_)
+        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3.*U_-2.*om0_**2 *V_ + 3.*r*dU_) - r*U_*dV)
+        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3.*U*V_+3.*U_*V-2.*om0_**2 *V_*V - 2.*om0**2 *V_*V \
+                + om2_**2*V_*V + om2**2 *V_*V + r*V*dU_ + r*V_*dU - r*U*dV_ - r*U_*dV - 2.*U_*U)
+        Bmm += self.wig_red(3,-2,-1)*om0*om0_*om2_*om3_*V_*V
+        Bmm += self.wig_red(-1,-2,3)*om0_*om0*om2*om3*V_*V   
 
         Bmm = 0.5*(((-1)**np.abs(1+m_))*prefac)[:,:,np.newaxis] \
                  * (Bmm/r**2)[np.newaxis,:,:]
         #tstamp('Bmm done')
 
         #B0- EXPRESSION
-        B0m = self.wig_red(1,-1,0)*om0_*(4*om0**2*V_*V + 8*U*U_ - 5*om0**2*U_*V - 3*r*om0**2*V*dV_ + 2*r**2*dU*dV_ - r*om0**2*V_*dV + \
-                r**2*V_*d2U + (-6.-2.*om0_**2 + om0**2)*U*V_ + r*U*(4*dV_ - r*d2V_))
-        B0m += self.wig_red(0,-1,1)*om0*(4*om0_**2*V*V_ + 8*U_*U - 5*om0_**2*U*V_ - 3*r*om0_**2*V_*dV + 2*r**2*dU_*dV - r*om0**2*V*dV_ + \
-                r**2*V*d2U_ + (-6. -2.*om0**2 + om0_**2)*U_*V + r*U_*(4*dV - r*d2V))
-        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*U_ - 4*V*V_ + 3*r*dV_*V + r*V_*dV)
-        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*U - 4*V_*V + 3*r*dV*V_ + r*V*dV_)
+        B0m = self.wig_red(1,-1,0)*om0_*(4.*om0**2 *V_*V + U_*(8.*U-5.*om0**2* V) - 3.*r*om0**2*V*dV_ \
+                + 2*r**2*dU*dV_ - r*om0**2 *V_*dV + r**2 *V_*d2U + U*((-6.-2.*om0_**2+om0**2)*V_ + r*(4.*dV_-r*d2V_)))
+        B0m += self.wig_red(0,-1,1)*om0*(4.*om0_**2 *V*V_ + U*(8.*U_-5.*om0_**2 *V_) - 3.*r*om0_**2 *V_*dV \
+                + 2*r**2*dU_*dV - r*om0_**2 *V*dV_ + r**2 *V*d2U_ + U_*((-6.-2.*om0**2+om0_**2)*V + r*(4.*dV-r*d2V)))
+        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*(U_-4.*V_+3.*r*dV_) + r*V_*dV)
+        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*(U-4.*V+3.*r*dV) + r*V*dV_)
 
         B0m = (0.25*((-1)**np.abs(m_))*prefac)[:,:,np.newaxis] \
                 * (B0m/r**2)[np.newaxis,:]
@@ -336,22 +334,20 @@ class Hkernels:
 #        exit()
 
         #B00 EXPRESSION
-        B00 = (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(2*(-1. + om0_**2)*V_*V - V_*(3*r*dU - 4*r*dV) - 2*r*U*dV_ - 2*r**2*dV_*dV)
-        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(2*(-1. + om0**2)*V*V_ - V*(3*r*dU_ - 4*r*dV_) - 2*r*U_*dV - 2*r**2*dV*dV_)
-        B00 += self.wig_red(0,0,0)*(-26*om0_**2*U*V_ + 12*om0_**2*om0**2*V_*V + 2*om0_**2*r*U*dV_ - 2*r*dU_*U + r**2*dU_*dU + r**2*U*d2U_ + 14*U*U_)
-        B00 += self.wig_red(0,0,0)*(-26*om0**2*U_*V + 12*om0**2*om0_**2*V*V_ + 2*om0**2*r*U_*dV - 2*r*dU*U_ + r**2*dU*dU_ + r**2*U_*d2U + 14*U_*U)
+        B00 = self.wig_red(0,0,0)*2.*(-2.*r*U*dU_ - 2.*r*U_*dU + om0**2*r*V*dU_ + om0_**2*r*V_*dU - 5.*om0_**2*V_*U \
+                - 5.*om0**2*V*U_ + 4.*om0**2*om0_**2*V_*V + om0_**2*r*U*dV_ + om0**2*r*U_*dV + 6.*U_*U)
+        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*(-om0_*om0)*(-U_*V-U*V_+2.*V_*V+r*V*dU_\
+                +r*V_*dU-2.*r*V*dV_-2*r*V_*dV+r*U*dV_+r*U_*dV+2*r**2 *dV_*dV)
 
-        B00 = (0.25*((-1)**np.abs(m_))*prefac)[:,:,np.newaxis] \
+        B00 = (0.5*((-1)**np.abs(m_))*prefac)[:,:,np.newaxis] \
                 * (B00/r**2)[np.newaxis,:]
         #tstamp('B00 done')
 
         #B+- EXPRESSION
-        Bpm = self.wig_red(0,0,0)*(4*om0_**2*U*V_ - 4*om0_**2*om0**2*V_*V - 10*r*U*dU_ + 6*r*om0**2*V*dU_ - 3*r**2*dU_*dU + 4*r*om0_**2*U*dV_ - r**2*U*d2U_)
-        Bpm += self.wig_red(0,0,0)*(4*om0**2*U_*V - 4*om0**2*om0_**2*V*V_ - 10*r*U_*dU + 6*r*om0_**2*V_*dU - 3*r**2*dU*dU_ + 4*r*om0**2*U_*dV - r**2*U_*d2U)
-        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0_*om0*om2_*om2*V_*V)
-        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0*om0_*om2*om2_*V*V_)
-        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(V_*U - 2*om0_**2*V_*V + r*U*dV_ + U_*U)
-        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(V*U_ - 2*om0**2*V*V_ + r*U_*dV + U*U_)
+        Bpm = self.wig_red(0,0,0)*2.*(-2.*r*dU_*U-2.*r*dU*U_+om0**2*r*dU_*V+om0_**2*r*dU*V_-2.*r**2*dU_*dU \
+                -om0_**2*U*V_-om0**2*U_*V+om0_**2*r*U*dV_+om0**2*r*U_*dV + 2.*U_*U)
+        Bpm += (self.wig_red(-2,0,2)+self.wig_red(2,0,-2))*(-4.*om0*om0_*om2*om2_*V_*V)
+        Bpm += (self.wig_red(-1,0,1)+self.wig_red(1,0,-1))*(om0*om0_)*(-r*V*dU_-r*V_*dU-V_*U-V*U_+r*U*dV_+r*U_*dV+2.*U_*U)
 
         Bpm = (0.25*((-1)**np.abs(m_))*prefac)[:,:,np.newaxis] \
                 * (Bpm/r**2)[np.newaxis,:]
@@ -460,12 +456,12 @@ class Hkernels:
         om3_ = om(l_,3)
 
         #B-- EXPRESSION
-        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3*U - 2*om0**2*V + 3*r*dU) - r*U*dV_)
-        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3*U_ - 2*om0_**2*V_ + 3*r*dU_) - r*U_*dV)
-        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3*U*V_ - 2*om0_**2*V_*V + om2_**2*V_*V + r*V*dU_ - r*U*dV_ - U_*U)
-        Bmm += self.wig_red(1,-2,1)*om0*om0_*(3*U_*V - 2*om0**2*V*V_ + om2**2*V*V_ + r*V_*dU - r*U_*dV - U*U_)
-        Bmm += self.wig_red(3,-2,-1)*om2_*om3_*om0*om0_*V_*V
-        Bmm += self.wig_red(-1,-2,3)*om2*om3*om0_*om0*V*V_
+        Bmm = self.wig_red(2,-2,0)*om0_*om2_*(V_*(3.*U-2.*om0**2 *V + 3.*r*dU) - r*U*dV_)
+        Bmm += self.wig_red(0,-2,2)*om0*om2*(V*(3.*U_-2.*om0_**2 *V_ + 3.*r*dU_) - r*U_*dV)
+        Bmm += self.wig_red(1,-2,1)*om0_*om0*(3.*U*V_+3.*U_*V-2.*om0_**2 *V_*V - 2.*om0**2 *V_*V \
+                + om2_**2*V_*V + om2**2 *V_*V + r*V*dU_ + r*V_*dU - r*U*dV_ - r*U_*dV - 2.*U_*U)
+        Bmm += self.wig_red(3,-2,-1)*om0*om0_*om2_*om3_*V_*V
+        Bmm += self.wig_red(-1,-2,3)*om0_*om0*om2*om3*V_*V   
         
         # np.save('prefac.npy',prefac)
 
@@ -475,12 +471,12 @@ class Hkernels:
         #tstamp('Bmm done')
 
         #B0- EXPRESSION
-        B0m = self.wig_red(1,-1,0)*om0_*(4*om0**2*V_*V + 8*U*U_ - 5*om0**2*U_*V - 3*r*om0**2*V*dV_ + 2*r**2*dU*dV_ - r*om0**2*V_*dV + \
-                r**2*V_*d2U + (-6.-2.*om0_**2 + om0**2)*U*V_ + r*U*(4*dV_ - r*d2V_))
-        B0m += self.wig_red(0,-1,1)*om0*(4*om0_**2*V*V_ + 8*U_*U - 5*om0_**2*U*V_ - 3*r*om0_**2*V_*dV + 2*r**2*dU_*dV - r*om0**2*V*dV_ + \
-                r**2*V*d2U_ + (-6. -2.*om0**2 + om0_**2)*U_*V + r*U_*(4*dV - r*d2V))
-        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*U_ - 4*V*V_ + 3*r*dV_*V + r*V_*dV)
-        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*U - 4*V_*V + 3*r*dV*V_ + r*V*dV_)
+        B0m = self.wig_red(1,-1,0)*om0_*(4.*om0**2 *V_*V + U_*(8.*U-5.*om0**2* V) - 3.*r*om0**2*V*dV_ \
+                + 2*r**2*dU*dV_ - r*om0**2 *V_*dV + r**2 *V_*d2U + U*((-6.-2.*om0_**2+om0**2)*V_ + r*(4.*dV_-r*d2V_)))
+        B0m += self.wig_red(0,-1,1)*om0*(4.*om0_**2 *V*V_ + U*(8.*U_-5.*om0_**2 *V_) - 3.*r*om0_**2 *V_*dV \
+                + 2*r**2*dU_*dV - r*om0_**2 *V*dV_ + r**2 *V*d2U_ + U_*((-6.-2.*om0**2+om0_**2)*V + r*(4.*dV-r*d2V)))
+        B0m += self.wig_red(-1,-1,2)*om0*om0_*om2*(U*V_ + V*(U_-4.*V_+3.*r*dV_) + r*V_*dV)
+        B0m += self.wig_red(2,-1,-1)*om0_*om0*om2_*(U_*V + V_*(U-4.*V+3.*r*dV) + r*V*dV_)
 
         B0m = (0.25*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (B0m/r**2)
 
@@ -490,21 +486,19 @@ class Hkernels:
 #        exit()
 
         #B00 EXPRESSION
-        B00 = (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(2*(-1. + om0_**2)*V_*V - V_*(3*r*dU - 4*r*dV) - 2*r*U*dV_ - 2*r**2*dV_*dV)
-        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(2*(-1. + om0**2)*V*V_ - V*(3*r*dU_ - 4*r*dV_) - 2*r*U_*dV - 2*r**2*dV*dV_)
-        B00 += self.wig_red(0,0,0)*(-26*om0_**2*U*V_ + 12*om0_**2*om0**2*V_*V + 2*om0_**2*r*U*dV_ - 2*r*dU_*U + r**2*dU_*dU + r**2*U*d2U_ + 14*U*U_)
-        B00 += self.wig_red(0,0,0)*(-26*om0**2*U_*V + 12*om0**2*om0_**2*V*V_ + 2*om0**2*r*U_*dV - 2*r*dU*U_ + r**2*dU*dU_ + r**2*U_*d2U + 14*U_*U)
+        B00 = self.wig_red(0,0,0)*2.*(-2.*r*U*dU_ - 2.*r*U_*dU + om0**2*r*V*dU_ + om0_**2*r*V_*dU - 5.*om0_**2*V_*U \
+                - 5.*om0**2*V*U_ + 4.*om0**2*om0_**2*V_*V + om0_**2*r*U*dV_ + om0**2*r*U_*dV + 6.*U_*U)
+        B00 += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*(-om0_*om0)*(-U_*V-U*V_+2.*V_*V+r*V*dU_\
+                +r*V_*dU-2.*r*V*dV_-2*r*V_*dV+r*U*dV_+r*U_*dV+2*r**2 *dV_*dV)
 
-        B00 = (0.25*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (B00/r**2)
+        B00 = (0.5*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (B00/r**2)
         #tstamp('B00 done')
 
         #B+- EXPRESSION
-        Bpm = self.wig_red(0,0,0)*(4*om0_**2*U*V_ - 4*om0_**2*om0**2*V_*V - 10*r*U*dU_ + 6*r*om0**2*V*dU_ - 3*r**2*dU_*dU + 4*r*om0_**2*U*dV_ - r**2*U*d2U_)
-        Bpm += self.wig_red(0,0,0)*(4*om0**2*U_*V - 4*om0**2*om0_**2*V*V_ - 10*r*U_*dU + 6*r*om0_**2*V_*dU - 3*r**2*dU*dU_ + 4*r*om0**2*U_*dV - r**2*U_*d2U)
-        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0_*om0*om2_*om2*V_*V)
-        Bpm += (self.wig_red(-2,0,2) + self.wig_red(2,0,-2))*(-2*om0*om0_*om2*om2_*V*V_)
-        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0_*om0*(V_*U - 2*om0_**2*V_*V + r*U*dV_ + U_*U)
-        Bpm += (self.wig_red(-1,0,1) + self.wig_red(1,0,-1))*om0*om0_*(V*U_ - 2*om0**2*V*V_ + r*U_*dV + U*U_)
+        Bpm = self.wig_red(0,0,0)*2.*(-2.*r*dU_*U-2.*r*dU*U_+om0**2*r*dU_*V+om0_**2*r*dU*V_-2.*r**2*dU_*dU \
+                -om0_**2*U*V_-om0**2*U_*V+om0_**2*r*U*dV_+om0**2*r*U_*dV + 2.*U_*U)
+        Bpm += (self.wig_red(-2,0,2)+self.wig_red(2,0,-2))*(-4.*om0*om0_*om2*om2_*V_*V)
+        Bpm += (self.wig_red(-1,0,1)+self.wig_red(1,0,-1))*(om0*om0_)*(-r*V*dU_-r*V_*dU-V_*U-V*U_+r*U*dV_+r*U_*dV+2.*U_*U)
 
 
         Bpm = (0.25*((-1)**np.abs(0))*prefac)[:,np.newaxis] * (Bpm/r**2)

supermatrix.py
@@ -53,140 +53,44 @@ omega_ref0 = np.mean(omega_nl)
 
 total_m = len(nl_list) + 2*np.sum(nl_list, axis = 0)[1]
 
-Z = np.zeros((total_m, total_m))
-Z_diag = np.identity(total_m)
-Z_dpt = np.zeros((total_m, total_m))
-mi_beg = 0
-for i in range(len(nl_list)):
-    mj_beg = 0
-    for j in range(len(nl_list)): 
-        tstamp()
-        n_,l_ = nl_list[i]
-        n,l = nl_list[j]
-
-        mi_end = mi_beg + (2*l_+1)
-        mj_end = mj_beg + (2*l+1)
-        print('((%i,%i),(%i,%i))'%(n_,l_,n,l))
-
-        #implementing selection rule: delta_l <= s_max
-        if(np.abs(l-l_)>s_max_DR): 
-            mj_beg += 2*l+1
-            print('Skipping')
-            continue
-        
-#        temp_matrix = submatrix.submatrix(n_,n,l_,l,r,45.,field_type)
-        temp_matrix = submatrix.diffrot(n_,n,l_,l,r,omega_ref0)
-        #print(temp_matrix)
-        temp_matrix = np.diag(temp_matrix)
-#        temp_matrix = np.diag(submatrix.submatrix_diagonal_diffrot(n_,l_,r,omega_nl[i]))
-        del_i, del_j = l_-np.amin([l_,l]), l-np.amin([l_,l])
-        temp_matrix = np.pad(temp_matrix,((del_i,del_i),(del_j,del_j)),'constant',\
-                      constant_values = 0)                
-        Z[mi_beg:mi_end,mj_beg:mj_end] = temp_matrix
-        
-        #tstamp('done in:')
-        mj_beg += 2*l+1
-                
-    Z_diag[mi_beg:mi_end,mi_beg:mi_end] *= -(omega_ref0**2 - omega_nl[i]**2)
-    Z_dpt[mi_beg:mi_end,mi_beg:mi_end] = Z[mi_beg:mi_end,mi_beg:mi_end]
-
-    mi_beg += 2*l_+1
-
-eig_vals_dpt = np.diag(Z_dpt)
-#inserting the diagonal component of the supermatrix
-Z += Z_diag   
-eig_vals_qdpt,eig_vts_qdpt = np.linalg.eig(Z)
-m_ind_arr = np.zeros(np.shape(eig_vals_qdpt),dtype=int)
-
-for i in range(len(eig_vals_qdpt)):
-    m_ind_arr[i] = np.argmax(np.abs(eig_vts_qdpt[i]))
-
-eig_vals_qdpt_arranged = eig_vals_qdpt[m_ind_arr]
-
-#######################
-#Extracting frequencies
-
-l_local = 0
-omega_nl_arr = np.zeros(total_m)
-for i in range(len(nl_list)):
-    omega_nl_arr[l_local:l_local + 2*nl_list[i,1]+1] = \
-                np.ones(2*nl_list[i,1]+1)*omega_nl[i]
-    l_local += 2*nl_list[i,1]+1
-
-f_dpt = (omega_nl_arr + eig_vals_dpt/(2*omega_nl_arr)) * OM *1e6
-f_qdpt = np.sqrt(omega_ref0**2 + eig_vals_qdpt_arranged) * OM *1e6
-
-# np.savetxt('./bdary_modes_analysis/%i.dat'%(len(nl_list)),f_dpt-f_qdpt)
-sys.exit()
-
-#generating plots for DPT and QDPT and comparing
-
-fn.plot_freqs(f_dpt,f_qdpt,nl_list,'DR',True)
-
-#Extracting the a-coefficients
-
-a_dpt = np.zeros((len(nl_list),j_max+1))
-a_qdpt = np.zeros((len(nl_list),j_max+1))
-
-l_local = 0
-for i in range(len(nl_list)):
-    del_omega_dpt = f_dpt[l_local:l_local+2*nl_list[i,1]+1] - omega_nl[i]*1e6*OM
-    del_omega_qdpt = f_qdpt[l_local:l_local+2*nl_list[i,1]+1] - omega_nl[i]*1e6*OM
-    a_dpt[i,:] = fn.a_coeff_GSO(del_omega_dpt,nl_list[i,1],j_max) 
-    a_qdpt[i,:] = fn.a_coeff_GSO(del_omega_qdpt,nl_list[i,1],j_max)
-    l_local += 2*nl_list[i,1] + 1
-
-# sys.exit()
-
-# #MAGNETIC PERTUBATION
-# Z = np.zeros((total_m, total_m),dtype='complex128')
-# Z_diag = np.identity(total_m,dtype='complex128')
-# Z_dpt = np.zeros((total_m, total_m),dtype='complex128')
-# f_DR = np.zeros(total_m)
+# Z = np.zeros((total_m, total_m))
+# Z_diag = np.identity(total_m)
+# Z_dpt = np.zeros((total_m, total_m))
 # mi_beg = 0
 # for i in range(len(nl_list)):
 #     mj_beg = 0
-#     for j in range(len(nl_list)):
+#     for j in range(len(nl_list)): 
+#         tstamp()
 #         n_,l_ = nl_list[i]
 #         n,l = nl_list[j]
-       
+
 #         mi_end = mi_beg + (2*l_+1)
 #         mj_end = mj_beg + (2*l+1)
 #         print('((%i,%i),(%i,%i))'%(n_,l_,n,l))
 
 #         #implementing selection rule: delta_l <= s_max
-#         if(np.abs(l-l_)>s_max_H): 
+#         if(np.abs(l-l_)>s_max_DR): 
 #             mj_beg += 2*l+1
 #             print('Skipping')
 #             continue
-       
-#         temp_matrix = submatrix.lorentz_diagonal(n_,n,l_,l,r,field_type,smoothen=smoothen)
-       
+        
+# #        temp_matrix = submatrix.submatrix(n_,n,l_,l,r,45.,field_type)
+#         temp_matrix = submatrix.diffrot(n_,n,l_,l,r,omega_ref0)
+#         #print(temp_matrix)
 #         temp_matrix = np.diag(temp_matrix)
+# #        temp_matrix = np.diag(submatrix.submatrix_diagonal_diffrot(n_,l_,r,omega_nl[i]))
 #         del_i, del_j = l_-np.amin([l_,l]), l-np.amin([l_,l])
 #         temp_matrix = np.pad(temp_matrix,((del_i,del_i),(del_j,del_j)),'constant',\
-#                       constant_values = 0)         
-#         Z[mi_beg:mi_end,mj_beg:mj_end] = temp_matrix                             
-       
-       
-#         mj_beg += 2*l+1
-       
-#     #Getting omega_nlm by DPT
-
-#     tstamp('omega_nlm starts')
-#     domega_nlm_sq = submatrix.diffrot(n_,n_,l_,l_,r,omega_nl[i])
-#     domega_nlm_sq = domega_nlm_sq.astype('complex128')
-#     omega_nlm_sq = domega_nlm_sq +  omega_nl[i]**2
-#     tstamp('omega_nlm ends')
-    
-#     f_DR[mi_beg:mi_end] = np.sqrt(np.real(omega_nlm_sq))
+#                       constant_values = 0)                
+#         Z[mi_beg:mi_end,mj_beg:mj_end] = temp_matrix
         
-#     Z_diag[mi_beg:mi_end,mi_beg:mi_end] *= np.diag(omega_nlm_sq - omega_ref0**2)
+#         #tstamp('done in:')
+#         mj_beg += 2*l+1
+                
+#     Z_diag[mi_beg:mi_end,mi_beg:mi_end] *= -(omega_ref0**2 - omega_nl[i]**2)
 #     Z_dpt[mi_beg:mi_end,mi_beg:mi_end] = Z[mi_beg:mi_end,mi_beg:mi_end]
 
 #     mi_beg += 2*l_+1
-
-
 
 # eig_vals_dpt = np.diag(Z_dpt)
 # #inserting the diagonal component of the supermatrix
@@ -212,19 +116,115 @@ for i in range(len(nl_list)):
 # f_dpt = (omega_nl_arr + eig_vals_dpt/(2*omega_nl_arr)) * OM *1e6
 # f_qdpt = np.sqrt(omega_ref0**2 + eig_vals_qdpt_arranged) * OM *1e6
 
-# #######################
+# # np.savetxt('./bdary_modes_analysis/%i.dat'%(len(nl_list)),f_dpt-f_qdpt)
+# sys.exit()
+
 # #generating plots for DPT and QDPT and comparing
 
-# fn.plot_freqs(np.real(f_dpt),np.real(f_qdpt),nl_list,'M',True,f_DR*1e6*OM)
+# fn.plot_freqs(f_dpt,f_qdpt,nl_list,'DR',True)
 
 # #Extracting the a-coefficients
 
 # a_dpt = np.zeros((len(nl_list),j_max+1))
 # a_qdpt = np.zeros((len(nl_list),j_max+1))
 
-# #taking only real parts of frequency matricesfn.a_coeff_GSO(del_omega_a,l,20)
-# f_dpt = np.real(f_dpt)
-# f_qdpt = np.real(f_qdpt)
+# l_local = 0
+# for i in range(len(nl_list)):
+#     del_omega_dpt = f_dpt[l_local:l_local+2*nl_list[i,1]+1] - omega_nl[i]*1e6*OM
+#     del_omega_qdpt = f_qdpt[l_local:l_local+2*nl_list[i,1]+1] - omega_nl[i]*1e6*OM
+#     a_dpt[i,:] = fn.a_coeff_GSO(del_omega_dpt,nl_list[i,1],j_max) 
+#     a_qdpt[i,:] = fn.a_coeff_GSO(del_omega_qdpt,nl_list[i,1],j_max)
+#     l_local += 2*nl_list[i,1] + 1
+
+# sys.exit()
+
+#MAGNETIC PERTUBATION
+Z = np.zeros((total_m, total_m),dtype='complex128')
+Z_diag = np.identity(total_m,dtype='complex128')
+Z_dpt = np.zeros((total_m, total_m),dtype='complex128')
+f_DR = np.zeros(total_m)
+mi_beg = 0
+for i in range(len(nl_list)):
+    mj_beg = 0
+    for j in range(len(nl_list)):
+        n_,l_ = nl_list[i]
+        n,l = nl_list[j]
+       
+        mi_end = mi_beg + (2*l_+1)
+        mj_end = mj_beg + (2*l+1)
+        print('((%i,%i),(%i,%i))'%(n_,l_,n,l))
+
+        #implementing selection rule: delta_l <= s_max
+        if(np.abs(l-l_)>s_max_H): 
+            mj_beg += 2*l+1
+            print('Skipping')
+            continue
+       
+        temp_matrix = submatrix.lorentz_diagonal(n_,n,l_,l,r,field_type,smoothen=smoothen)
+       
+        temp_matrix = np.diag(temp_matrix)
+        del_i, del_j = l_-np.amin([l_,l]), l-np.amin([l_,l])
+        temp_matrix = np.pad(temp_matrix,((del_i,del_i),(del_j,del_j)),'constant',\
+                      constant_values = 0)         
+        Z[mi_beg:mi_end,mj_beg:mj_end] = temp_matrix                             
+       
+       
+        mj_beg += 2*l+1
+       
+    #Getting omega_nlm by DPT
+
+    tstamp('omega_nlm starts')
+    domega_nlm_sq = submatrix.diffrot(n_,n_,l_,l_,r,omega_nl[i])
+    domega_nlm_sq = domega_nlm_sq.astype('complex128')
+    omega_nlm_sq = domega_nlm_sq +  omega_nl[i]**2
+    tstamp('omega_nlm ends')
+    
+    f_DR[mi_beg:mi_end] = np.sqrt(np.real(omega_nlm_sq))
+        
+    Z_diag[mi_beg:mi_end,mi_beg:mi_end] *= np.diag(omega_nlm_sq - omega_ref0**2)
+    Z_dpt[mi_beg:mi_end,mi_beg:mi_end] = Z[mi_beg:mi_end,mi_beg:mi_end]
+
+    mi_beg += 2*l_+1
+
+
+
+eig_vals_dpt = np.diag(Z_dpt)
+#inserting the diagonal component of the supermatrix
+Z += Z_diag   
+eig_vals_qdpt,eig_vts_qdpt = np.linalg.eig(Z)
+m_ind_arr = np.zeros(np.shape(eig_vals_qdpt),dtype=int)
+
+for i in range(len(eig_vals_qdpt)):
+    m_ind_arr[i] = np.argmax(np.abs(eig_vts_qdpt[i]))
+
+eig_vals_qdpt_arranged = eig_vals_qdpt[m_ind_arr]
+
+#######################
+#Extracting frequencies
+
+l_local = 0
+omega_nl_arr = np.zeros(total_m)
+for i in range(len(nl_list)):
+    omega_nl_arr[l_local:l_local + 2*nl_list[i,1]+1] = \
+                np.ones(2*nl_list[i,1]+1)*omega_nl[i]
+    l_local += 2*nl_list[i,1]+1
+
+f_dpt = (omega_nl_arr + eig_vals_dpt/(2*omega_nl_arr)) * OM *1e6
+f_qdpt = np.sqrt(omega_ref0**2 + eig_vals_qdpt_arranged) * OM *1e6
+
+#######################
+#generating plots for DPT and QDPT and comparing
+
+fn.plot_freqs(np.real(f_dpt),np.real(f_qdpt),nl_list,'M',True,f_DR*1e6*OM)
+
+#Extracting the a-coefficients
+
+a_dpt = np.zeros((len(nl_list),j_max+1))
+a_qdpt = np.zeros((len(nl_list),j_max+1))
+
+#taking only real parts of frequency matricesfn.a_coeff_GSO(del_omega_a,l,20)
+f_dpt = np.real(f_dpt)
+f_qdpt = np.real(f_qdpt)
 
 # l_local = 0
 # for i in range(len(nl_list)):